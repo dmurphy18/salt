0eaa83ff2697296642703b8b3be7f662e047cbf1
David Murphy <dmurphy@saltstack.com>
2020-07-07 13:09:52 -0600

    Futher debug statements

--- salt/modules/status.py
+++ salt/modules/status.py
@@ -1714,17 +1714,19 @@ def proxy_reconnect(proxy_name, opts=None):
 
         salt '*' status.proxy_reconnect rest_sample
     '''
+    log.debug("DGM proxy_reconnect entry, proxy_name '{0}'".format(proxy_name))
 
     if not opts:
         opts = __opts__
 
     if 'proxy' not in opts:
+        log.debug("DGM proxy_reconnect proxy_name '{0}', proxy not in opts exit, opts '{1}'".format(proxy_name, opts))
         return False  # fail
 
     proxy_keepalive_fn = proxy_name+'.alive'
-    log.debug("DGM proxy_reconnect proxy_keepalive_fn '{0}'".format(proxy_keepalive_fn))
+    log.debug("DGM proxy_reconnect proxy_keepalive_fn '{0}', __proxy__".format(proxy_keepalive_fn))
     if proxy_keepalive_fn not in __proxy__:
-        log.debug("DGM proxy_reconnect proxy_keepalive_fn '{0}' not in __proxy__".format(proxy_keepalive_fn))
+        log.debug("DGM proxy_reconnect proxy_keepalive_fn '{0}' not in  __proxy__ '{1}'".format(proxy_keepalive_fn, __proxy__))
         return False  # fail
 
     is_alive = __proxy__[proxy_keepalive_fn](opts)
