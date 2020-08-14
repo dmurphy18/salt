18076ed4054c1fd4582714d6805ba5bd77ae3672
David Murphy <dmurphy@saltstack.com>
2020-08-14 16:47:10 -0600

    Debugging reboot

--- salt/modules/junos.py
+++ salt/modules/junos.py
@@ -1473,21 +1473,21 @@ def install_os(path=None, **kwargs):
             ## conn.sw.reboot(**reboot_kwargs)
 
             __proxy__["junos.reboot_active"]()
-            log.debug("DGM install_os reboot True, pre conn.sw.reboot, reboot_active flag '{1}'".format(__proxy__["junos.get_reboot_active"]()))
+            log.debug("DGM install_os reboot True, pre conn.sw.reboot, reboot_active flag '{0}'".format(__proxy__["junos.get_reboot_active"]()))
             conn.sw.reboot(in_min=0)
             log.debug("DGM install_os reboot True, post conn.sw.reboot")
             __proxy__["junos.reboot_clear"]()
-            log.debug("DGM install_os reboot True, post conn.sw.reboot, reboot_active flag '{1}'".format(__proxy__["junos.get_reboot_active"]()))
+            log.debug("DGM install_os reboot True, post conn.sw.reboot, reboot_active flag '{0}'".format(__proxy__["junos.get_reboot_active"]()))
 
         except ConnectClosedError as connclosed:  # pylint: disable=broad-except
             log.debug("DGM install_os reboot True, post conn.sw.reboot ConnectClosedError exception '{0}'".format(connclosed))
             __proxy__["junos.reboot_clear"]()
-            log.debug("DGM install_os reboot True, post conn.sw.reboot, reboot_active flag '{1}'".format(__proxy__["junos.get_reboot_active"]()))
+            log.debug("DGM install_os reboot True, post conn.sw.reboot, reboot_active flag '{0}'".format(__proxy__["junos.get_reboot_active"]()))
 
         except Exception as exception:  # pylint: disable=broad-except
             log.debug("DGM install_os reboot True, post conn.sw.reboot exception '{0}'".format(exception))
             __proxy__["junos.reboot_clear"]()
-            log.debug("DGM install_os reboot True, post conn.sw.reboot, reboot_active flag '{1}'".format(__proxy__["junos.get_reboot_active"]()))
+            log.debug("DGM install_os reboot True, post conn.sw.reboot, reboot_active flag '{0}'".format(__proxy__["junos.get_reboot_active"]()))
             ret[
                 "message"
             ] = "Installation successful but reboot failed due to : '{0}'".format(
